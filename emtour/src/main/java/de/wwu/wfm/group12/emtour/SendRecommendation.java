086b906e3e7d88902ca3ee2c984c81d316857d45
pedro.marangoni <pedro911@gmail.com>
2017-07-16 22:43:33 +0200

    changes into bpmn model, stuck on send http message

--- emtour/src/main/java/de/wwu/wfm/group12/emtour/SendRecommendation.java
+++ emtour/src/main/java/de/wwu/wfm/group12/emtour/SendRecommendation.java
@@ -5,6 +5,17 @@ import java.util.regex.Pattern;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.connect.Connectors;
+import org.camunda.connect.httpclient.HttpConnector;
+import org.camunda.connect.httpclient.HttpRequest;
+import org.camunda.connect.httpclient.HttpResponse;
+
+import connectjar.org.apache.http.client.HttpClient;
+import connectjar.org.apache.http.client.methods.HttpUriRequest;
+import connectjar.org.apache.http.client.methods.RequestBuilder;
+import connectjar.org.apache.http.impl.client.HttpClients;
+
+
 
 public class SendRecommendation implements JavaDelegate {
 	private final static Logger LOGGER = Logger.getLogger("RECOMMENDATIONS-REQUESTS");
@@ -29,7 +40,47 @@ public class SendRecommendation implements JavaDelegate {
 		        "value": "false"
 		    }}
 		}*/
-
+		
+	/*	// build HTTP request with all variables as parameters
+		HttpClient client = HttpClients.createDefault();
+		RequestBuilder requestBuilder = RequestBuilder.get()
+		.setUri("http://192.168.1.30:8080/engine-rest/request-received")
+		.addParameter("customerid", customerId);
+		for (String variable : execution.getVariableNames()) {
+			requestBuilder.addParameter(variable, String.valueOf(execution.getVariable(variable)));
+		}
+		// execute request
+		HttpUriRequest request = requestBuilder.build();
+		HttpResponse response = client.execute(request);
+		// log debug information
+		System.out.println(request.getURI());
+		System.out.println(response.getStatusLine());
+		
+		HttpConnector http = Connectors.getConnector(HttpConnector.ID);
+		http.createRequest()
+		  .get()
+		  .url("http://camunda.org")
+		  .execute();
+		
+		HttpResponse response = http.createRequest()
+				  .get()
+				  .header("Accept", "application/json")
+				  .url("http://camunda.org")
+				  .execute();
+		HttpRequest request = http.createRequest();
+		request.setRequestParameter("method", "GET");
+		request.setRequestParameter("url", "http://camunda.org");
+		request.setRequestParameter("payload", "hello world!");
+		System.out.println(request.getUrl());
+		Integer statusCode = response.getStatusCode();
+		String contentTypeHeader = response.getHeader("Content-Type");
+		String body = response.getResponse();
+		response.getResponseParameter("statusCode");
+		response.getResponseParameter("headers");
+		response.getResponseParameter("response");
+		//System.out.println(response.getStatusLine());
+		  
+		 */
 	}
 
 }
