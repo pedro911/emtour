359d70456281db033bc415b55a3aa7543b804142
ZainabRao <xainab.rao@gmail.com>
2017-07-17 19:37:18 +0500

    zainab works

--- emtour/src/main/java/de/wwu/wfm/group12/emtour/ProcessBill.java
+++ emtour/src/main/java/de/wwu/wfm/group12/emtour/ProcessBill.java
@@ -1,5 +1,7 @@
 package de.wwu.wfm.group12.emtour;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
@@ -12,8 +14,25 @@ public class ProcessBill implements JavaDelegate {
 	 private final static Logger LOGGER = Logger.getLogger("RECOMMENDATIONS-REQUESTS");
 
 	  public void execute(DelegateExecution execution) throws Exception {
-	    LOGGER.info("Process Bill '" 
-	    		+ execution.getVariable("name") + "' age: '"
-	    		+ execution.getVariable("age") + "'...");
+	    	    
+	   int activitySetPrice = 100;
+	   int bill;
+	   
+	   int id = (Integer)execution.getVariable("id");
+	   int children = ((Integer) execution.getVariable("children")).intValue();
+	   int adult = (Integer) execution.getVariable("adult");
+	   int number_of_days = (Integer) execution.getVariable("number_of_days");
+	   int budget = (Integer) execution.getVariable("budget");
+	   
+	// Generate bill depending on client's budget & activity set price provided from FunSpark
+	   bill = budget * adult + (budget/2) * children + activitySetPrice;  
+	   
+	   System.out.println("Total Bill is: " + bill + " Persons: " + String.valueOf(adult +children) + " Country: " + execution.getVariable("country"));
+	   
+	  /* LOGGER.info("Total Bill is:'" 
+	    		+ bill + "' Country: '"
+	    		+ execution.getVariable("country") + "'");*/
+	   
 	  }
+}
 }
