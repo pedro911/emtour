daddb9b3e69d4c850e8f3b21bdbe5c59640cc626
pedro.marangoni <pedro911@gmail.com>
2017-07-18 16:41:34 +0200

    changes in process.bpmn

--- emtour/src/main/java/de/wwu/wfm/group12/emtour/ProcessBill.java
+++ emtour/src/main/java/de/wwu/wfm/group12/emtour/ProcessBill.java
@@ -1,16 +1,12 @@
 package de.wwu.wfm.group12.emtour;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.logging.Logger;
-
-import javax.ejb.Stateless;
-import javax.inject.Named;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 public class ProcessBill implements JavaDelegate {
+	
 	 private final static Logger LOGGER = Logger.getLogger("RECOMMENDATIONS-REQUESTS");
 
 	  public void execute(DelegateExecution execution) throws Exception {
@@ -34,5 +30,4 @@ public class ProcessBill implements JavaDelegate {
 	    		+ execution.getVariable("country") + "'");*/
 	   
 	  }
-}
 }
--- emtour/src/main/java/de/wwu/wfm/group12/emtour/SendRecommendation.java
+++ emtour/src/main/java/de/wwu/wfm/group12/emtour/SendRecommendation.java
@@ -19,74 +19,20 @@ public class SendRecommendation implements JavaDelegate {
 		LOGGER.info("Send Recommendations '" + execution.getVariable("name") + "' country: '"
 				+ execution.getVariable("country") + "'...");
 
-		// create db record with customer info
+		// create db record with customer info and return customerID
 		CustomerDatabase cdb = new CustomerDatabase();
 		cdb.createCustomerDbRecord(execution);
 		String customerId = cdb.lastCustomerId();
 		
-		// implement REST API to send recommendations to Funspark
-		/*
-		{
-		    "businessKey": "Instance_001",
-		    "messageName": "Message_AddInf",
-		    "processInstanceId": "7cc0d35d-680b-11e7-a49f-846920524153",
-		    "processVariables": {"Healthy": {
-		        "type": "Boolean",
-		        "value": "false"
-		    }}
-		}*/
-		/*
-		// build HTTP request with all variables as parameters
-		HttpClient client = HttpClients.createDefault();
-		RequestBuilder requestBuilder = RequestBuilder.get()
-		.setUri("http://192.168.1.30:8080/engine-rest/request-received")
-		.addParameter("customerid", customerId);
-		for (String variable : execution.getVariableNames()) {
-			requestBuilder.addParameter(variable, String.valueOf(execution.getVariable(variable)));
-		}
-		// execute request
-		HttpUriRequest request = requestBuilder.build();
-		HttpResponse response = client.execute(request);
-		// log debug information
-		System.out.println(request.getURI());
-		System.out.println(response.getStatusLine());
 		
-			
-		HttpConnector http = Connectors.getConnector(HttpConnector.ID);
-		http.createRequest()
-		  .post()
-		  .url("http://camunda.org")
-		  .execute();
-		
-		HttpResponse response = http.createRequest()
-				  .get()
-				  .header("Accept", "application/json")
-				  .url("http://192.168.1.30:8080/engine-rest/process-definition/key/funspark-id/start")
-				  .execute();
-		
-		
-		HttpRequest request = http.createRequest();
-		HttpResponse response = request.getR
-		request.setRequestParameter("method", "GET");
-		request.setRequestParameter("url", "192.168.1.30:8080/engine-rest/authorization");
-		request.setRequestParameter("payload", "hello world!");
-		System.out.println(request.getUrl());
-		Integer statusCode = response.getStatusCode();
-		String contentTypeHeader = response.getHeader("Content-Type");
-		String body = response.getResponse();
-		response.getResponseParameter("statusCode");
-		response.getResponseParameter("headers");
-		response.getResponseParameter("response");
-		//System.out.println(response.getStatusLine());
-		 */
-		
+		// RESTeasy Jboss API to send recommendations to Funspark
+				
 		String input;
 		ClientRequest request = new ClientRequest("http://192.168.1.30:8080/engine-rest/process-definition/key/funspark-id/start");
-		// "http://requestb.in/w1pjs5w1");
 		request.accept("application/json");
 		
 		input = "{\"variables\":{ "
-				+ "\"name\":{\"value\":\""+execution.getVariable("name")+"\", \"type\": \"String\"},"
+				+ "\"name\":{\"value\":\""+execution.getVariable("name")+"\", \"type\": \"String\"},"				
 				+ "\"desiredCity\":{\"value\":\""+execution.getVariable("desiredCity")+"\", \"type\": \"String\"}},"
 				+ "\"businessKey\":\""+execution.getId()+"\"}";
 		
